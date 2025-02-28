d878082b256d4fd468ea890d8a2826fe9e970627
frokanic <84749437+frokanic@users.noreply.github.com>
2025-02-28 22:32:45 +0200

    Initial commit

---
+++ app/src/main/java/com/frokanic/thebeautyofcompose/screen/basicanimations/RotatingBox.kt
@@ -0,0 +1,59 @@
+package com.frokanic.thebeautyofcompose.screen.basicanimations
+
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RotatingBox() {
+
+    val transition = rememberInfiniteTransition()
+    val rotation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(3_000),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .graphicsLayer(rotationZ = rotation)
+                    .size(100.dp)
+                    .background(color = Color.Red)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RotatingBoxPreview() {
+    RotatingBox()
+}
